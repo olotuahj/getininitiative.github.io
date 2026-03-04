c42dc5ea6e3e9c6b63ad6b9758f6f3539ce3cb0a
olotuah joseph <olotuahtobiloba@gmail.com>
2026-03-04 07:09:29 -0800

    bla

---
+++ script6.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const hamburger = document.getElementById('hamburger');
+    const mobileMenu = document.getElementById('MobileMenu');
+    const closeMenu = document.getElementById('closeMenu');
+
+    // open menu
+    hamburger.addEventListener('click', () => {
+        mobileMenu.classList.add('active');
+    });
+
+    // close menu
+    closeMenu.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+    });
+
+       // Fade-up AND Fade-left animations
+    const faders = document.querySelectorAll('.fade-up, .fade-left');
+
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show'); 
+                observer.unobserve(entry.target);  
+            }
+        });
+    }, { threshold: 0.3 });
+
+    faders.forEach(fader => appearOnScroll.observe(fader));
+});
